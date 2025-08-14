7cdde657bf9116e02a71fd724f49fd076fffd89f
Eugene <fabasoad@gmail.com>
2025-08-15 03:57:24 +0900

    feat: include 2.1.25-M* artifacts (#10)
    * feat: include 2.1.25-M* artifacts
    * fix: version to use as a threshold

--- lib/utils.bash
+++ lib/utils.bash
@@ -25,13 +25,9 @@ get_platform() {
 	echo -n $platform
 }
 
-case "$(get_arch)-$(get_platform)" in
-aarch64-pc-linux | aarch64-apple-darwin) GH_REPO="https://github.com/VirtusLab/coursier-m1" ;;
-*) GH_REPO="https://github.com/coursier/coursier" ;;
-esac
-
 TOOL_NAME="coursier"
 TOOL_TEST="coursier --help"
+GH_REPO="https://github.com/coursier/coursier"
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
@@ -45,9 +41,45 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 fi
 
 list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//'
+	list_all_github_tags_coursier_m1() {
+		git ls-remote --tags --refs --sort=version:refname "https://github.com/VirtusLab/coursier-m1"
+	}
+
+	list_all_github_tags_coursier_main() {
+		git ls-remote --tags --refs --sort=version:refname "$GH_REPO"
+	}
+
+	list_github_tags_aarch64_m1() {
+		SHA_2_1_25_M1="19ecf34cba47626df35a6fa462753f5ace6aa4c0"
+		list_all_github_tags_coursier_m1 | awk -v sha="$SHA_2_1_25_M1" '
+			{print}
+			$1 == sha {exit}
+    '
+	}
+
+	list_github_tags_aarch64_main() {
+		SHA_2_1_25_M2="bc7dc91fbb7cc70db69b1c386ba5ee208f45f1c7"
+		list_all_github_tags_coursier_main | awk -v sha="$SHA_2_1_25_M2" '
+			$1 == sha {found=1}
+			found
+		'
+	}
+
+	case "$(get_arch)-$(get_platform)" in
+	aarch64-pc-linux | aarch64-apple-darwin)
+		list_github_tags_aarch64_m1 |
+			grep -o 'refs/tags/.*' | cut -d/ -f3- |
+			sed 's/^v//'
+		list_github_tags_aarch64_main |
+			grep -o 'refs/tags/.*' | cut -d/ -f3- |
+			sed 's/^v//'
+		;;
+	*)
+		list_all_github_tags_coursier_main |
+			grep -o 'refs/tags/.*' | cut -d/ -f3- |
+			sed 's/^v//'
+		;;
+	esac
 }
 
 sort_versions() {
@@ -61,12 +93,38 @@ list_all_versions() {
 	# Change this function if coursier has other means of determining installable versions.
 	list_github_tags
 }
+
 download_release() {
+	get_repo() {
+		case "$(get_arch)-$(get_platform)" in
+		aarch64-pc-linux | aarch64-apple-darwin)
+			major=$(echo "$1" | cut -d '.' -f 1)
+			minor=$(echo "$1" | cut -d '.' -f 2)
+			patch=$(echo "$1" | cut -d '.' -f 3 | cut -d '-' -f 1)
+			rc=$(echo "$1" | awk -F 'M' '{if (NF > 1) print $2; else print ""}')
+			if [ "$major" -lt 2 ]; then
+				echo "https://github.com/VirtusLab/coursier-m1"
+			elif [ "$major" -eq 2 ] && [ "$minor" -lt 1 ]; then
+				echo "https://github.com/VirtusLab/coursier-m1"
+			elif [ "$major" -eq 2 ] && [ "$minor" -eq 1 ] && [ "$patch" -lt 25 ]; then
+				echo "https://github.com/VirtusLab/coursier-m1"
+			elif [ "$major" -eq 2 ] && [ "$minor" -eq 1 ] && [ "$patch" -eq 25 ] && [ -n "$rc" ] && [ "$rc" -lt 2 ]; then
+				echo "https://github.com/VirtusLab/coursier-m1"
+			else
+				echo "$GH_REPO"
+			fi
+			;;
+		*)
+			echo "$GH_REPO"
+			;;
+		esac
+	}
+
 	local version filename url
 	version="$1"
 	filename="$2"
 
-	url="$GH_REPO/releases/download/v${version}/cs-$(get_arch)-$(get_platform)"
+	url="$(get_repo "$version")/releases/download/v${version}/cs-$(get_arch)-$(get_platform)"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	if [ "$(get_platform)" == "pc-win32" ]; then
